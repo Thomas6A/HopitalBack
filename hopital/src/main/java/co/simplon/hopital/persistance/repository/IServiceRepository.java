743c9493a824fcff73ddba320e18fc653b55d5a1
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-26 10:00:18 +0100

    feat(business) : add Service DTO

--- hopital/src/main/java/co/simplon/hopital/persistance/repository/IServiceRepository.java
+++ hopital/src/main/java/co/simplon/hopital/persistance/repository/IServiceRepository.java
@@ -1,9 +1,11 @@
 package co.simplon.hopital.persistance.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import co.simplon.hopital.persistance.entity.Service;
 
+@Repository
 public interface IServiceRepository extends JpaRepository<Service, Integer> {
 
 }
