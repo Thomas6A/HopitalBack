843f01fb6b06438c6c7f871213b2d30bea55b5e6
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-29 10:04:11 +0100

    docs(persistance) : add javadoc on entity

--- hopital/src/main/java/co/simplon/hopital/persistance/entity/Bed.java
+++ hopital/src/main/java/co/simplon/hopital/persistance/entity/Bed.java
@@ -9,6 +9,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+/**
+ * Represents a bed in a hospital room.
+ */
 @Entity
 @Table(name = "bed")
 public class Bed {
@@ -25,26 +28,56 @@ public class Bed {
 	@JoinColumn(name = "room_id", nullable = false)
 	private Room room;
 
+	/**
+	 * Gets the ID of the bed.
+	 * 
+	 * @return The bed ID.
+	 */
 	public int getBedId() {
 		return bedId;
 	}
 
+	/**
+	 * Sets the ID of the bed.
+	 * 
+	 * @param bedId The bed ID to set.
+	 */
 	public void setBedId(int bedId) {
 		this.bedId = bedId;
 	}
 
+	/**
+	 * Checks if the bed is occupied or available.
+	 * 
+	 * @return True if the bed is occupied, false if it's available.
+	 */
 	public boolean isBedStatut() {
 		return bedStatut;
 	}
 
+	/**
+	 * Sets the status of the bed (occupied or available).
+	 * 
+	 * @param bedStatut The status of the bed to set.
+	 */
 	public void setBedStatut(boolean bedStatut) {
 		this.bedStatut = bedStatut;
 	}
 
+	/**
+	 * Gets the room associated with the bed.
+	 * 
+	 * @return The room associated with the bed.
+	 */
 	public Room getRoom() {
 		return room;
 	}
-
+	
+	/**
+	 * Sets the room associated with the bed.
+	 * 
+	 * @param room The room to associate with the bed.
+	 */
 	public void setRoom(Room room) {
 		this.room = room;
 	}
--- hopital/src/main/java/co/simplon/hopital/persistance/entity/Service.java
+++ hopital/src/main/java/co/simplon/hopital/persistance/entity/Service.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+/**
+ * Represents a service in a hospital.
+ */
 @Entity
 @Table(name="service")
 public class Service {
@@ -19,18 +22,38 @@ public class Service {
 	@Column(nullable = false, length = 50, name = "service_name")
 	private String serviceName;
 
+	/**
+	 * Gets the ID of the service.
+	 * 
+	 * @return The service ID.
+	 */
 	public int getServiceId() {
 		return serviceId;
 	}
 
+	/**
+	 * Sets the ID of the service.
+	 * 
+	 * @param serviceId The service ID to set.
+	 */
 	public void setServiceId(int serviceId) {
 		this.serviceId = serviceId;
 	}
 
+	/**
+	 * Gets the name of the service.
+	 * 
+	 * @return The service name.
+	 */
 	public String getServiceName() {
 		return serviceName;
 	}
 
+	/**
+	 * Sets the name of the service.
+	 * 
+	 * @param serviceName The service name to set.
+	 */
 	public void setServiceName(String serviceName) {
 		this.serviceName = serviceName;
 	}
