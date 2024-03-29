e11081f3109dd141dd927843a0016fda95f14e08
Thomas6A <thomas.baudrin@outlook.fr>
2024-03-29 10:07:06 +0100

    docs(persistance) : add javadoc on repository

--- hopital/src/main/java/co/simplon/hopital/persistance/repository/IBedRepository.java
+++ hopital/src/main/java/co/simplon/hopital/persistance/repository/IBedRepository.java
@@ -8,9 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import co.simplon.hopital.persistance.entity.Bed;
 
+/**
+ * Repository interface for managing Bed entities.
+ */
 @Repository
 public interface IBedRepository extends JpaRepository<Bed, Integer> {
 	
+	/**
+	 * Retrieves a list of free beds.
+	 * 
+	 * @return A list of free beds.
+	 */
 	@Query("select b from Bed b where bedStatut = false")
 	public List<Bed> getFreeBed();
 }
